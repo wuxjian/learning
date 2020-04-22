5f47a2c78b2776d4accaa9e7ab6909e8b7c70e27
wuxjian <wu_xjian@163.com>
2020-04-22 15:06:11 +0800

    '项目初始化'

---
+++ src/main/java/the/school/learning/controller/IndexController.java
@@ -0,0 +1,21 @@
+package the.school.learning.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+@Controller
+public class IndexController {
+
+    //首页 /index.html 或 空
+    @GetMapping({"/index.html", ""})
+    public String index(Model model) {
+        model.addAttribute("name", "张三");
+        return "index";
+    }
+
+
+
+}
