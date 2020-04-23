247a949993161076c2c1c34816554fe8ea33799c
wuxjian <wu_xjian@163.com>
2020-04-23 18:00:01 +0800

    路径hash

--- src/main/resources/static/assets/js/index.js
+++ src/main/resources/static/assets/js/index.js
@@ -1,32 +1,32 @@
 var app = {
     global: {
-        currentUrl: '/user/profile.html'
+
     },
     init: function () {
         this.initBinding();
         this.firstLoad();
     },
     initBinding: function () {
+        window.onhashchange=function(event){
+            var newURL = event.newURL;
+            var hash = newURL.substring(newURL.lastIndexOf("#") + 1);
+            var url = '/' + hash + '.html';
+            $('#main').load(url);
+        };
         $('#logout').click(function () {
             $.get('/logout');
             location.href = "/login.html";
         });
         //菜单点击事件
         $('#sidebar-menu .child_menu li').click(function () {
-            var clickUrl = $(this).attr('data-url');
-            if (clickUrl  === app.global.currentUrl){
-                return
-            }
-            app.global.currentUrl = clickUrl;
             $('#sidebar-menu .child_menu li').removeClass('active');
             $(this).addClass('active');
-            $('#main').load(clickUrl);
         });
-        $('.first-menu').click();
+
     },
     //第一次加载
     firstLoad: function () {
-        $('#main').load(this.global.currentUrl);
+        $('#main').load('/user/profile.html');
     }
 
 }
